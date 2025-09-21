4b565869520fb40e3ac7245c0a24f382c83a1663
Ananya D R <ananyadr2004@gmail.com>
2025-09-21 18:23:29 +0530

    All required files are uploaded

---
+++ script.js
@@ -0,0 +1,11 @@
+function button_alert(){
+    alert("Hi all! Welcome to my personal webpage.")
+}
+
+function hover_color_change(){
+    document.body.style.backgroundColor="lightblue";
+}
+
+function reset_color_change(){
+    document.body.style.backgroundColor="lightyellow";
+}
